5bb946a2685418705025bc51f787b39ca9a434ab
lishuai <lishuai@zuoyebang.com>
2020-06-27 23:33:55 +0800

    feat: 404 error

--- src/app.js
+++ src/app.js
@@ -10,11 +10,21 @@ const logger = require('koa-logger')
 
 const { REDIS_CONF } = require('./conf/db')
 
+const {isProd} = require('./utils/env')
+
 const index = require('./routes/index')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let onerrorConf = {}
+if (isProd) {
+  onerrorConf = {
+    redirect: '/error'
+  }
+}
+
+onerror(app, onerrorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -42,17 +52,12 @@ app.use(session({ // 如果不用就不会往session里写数据
     all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
   })
 }))
-// logger
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
+
 
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
